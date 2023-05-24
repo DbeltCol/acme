b456dc0647f2576b3289650b0b432ac70f302614
diego beltran <dbeltrandev@gmail.com>
2023-05-24 01:21:12 -0500

    implementing template

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path:'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
+  },
+
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full',
+  },
+
+  {
+    path:'**',
+    redirectTo:'/error/404',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  URL_SERVICES: "http://127.0.0.1:8002/api/",
+  URL_BACKEND: "http://127.0.0.1:8002/",
+  URL_FRONTEND:"http://127.0.0.1:4200/"
+}
